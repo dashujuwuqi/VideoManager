be27d0a19a4fdcce7f46df22b4f3eb9ab6717070
675514037 <675514037@qq.com>
2019-08-15 19:36:58 +0800

    luqiming 第一次提交

--- src/main/java/com/zhiyou/backgroundservice/AdminService.java
+++ src/main/java/com/zhiyou/backgroundservice/AdminService.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
 public interface AdminService {
+	//111
     long countByExample(AdminExample example);
 
     int deleteByExample(AdminExample example);
